a1ff57790550c2155b4a2c96a5688b39c7510c21
Brent Foster <brent.m.foster@me.com>
2025-09-07 16:39:08 -0400

    Update Jest config and workflow for custom test results
    Configures Jest and the deploy workflow to output test results to './test-results/js-junit.xml' using jest-junit. Removes deprecated 'testResultsProcessor' from Jest config and ensures the workflow checks for the correct output file.

--- summary-event-lambda/jest.config.js
+++ summary-event-lambda/jest.config.js
@@ -2,6 +2,5 @@ module.exports = {
     testEnvironment: 'node',
     reporters: [
         'default', ['jest-junit', { outputDirectory: './test-results', outputName: 'js-junit.xml' }]
-    ],
-    testResultsProcessor: 'jest-junit'
+    ]
 };
